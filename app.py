f3099a05ca35e4e4d04e52ebe68f251235056989
Raghavasai Kowlasker <rkowlasker@unomaha.edu>
2022-06-27 14:30:06 -0500

    updated app.py

--- app.py
+++ app.py
@@ -4,8 +4,9 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def hello_world(
+):
+    return "Hello world!"
 
 
 @app.route('/about')
@@ -16,14 +17,21 @@ def welcome():
 @app.route('/webhook', methods=['POST'])
 def webhook():
     req = request.get_json(silent=True, force=True)
-    print(req)
-
-    return {
-        'fulfillmentText': 'Hello from the other side.'
-    }
-
-
-if __name__ == "__main__":
-    app.secret_key = 'ItIsASecret'
-    app.debug = True
-    app.run()
+    fulfillmentText = ''
+    sum = 0
+    query_result = req.get('queryResult')
+    if query_result.get('action') == 'add.numbers':
+        num1 = int(query_result.get('parameters').get('number'))
+        num2 = int(query_result.get('parameters').get('number1'))
+        sum = str(num1 + num2)
+        print('here num1 = {0}'.format(num1))
+        print('here num2 = {0}'.format(num2))
+        fulfillmentText = 'The sum of the two numbers is ' + sum
+    elif query_result.get('action') == 'multiply.numbers':
+        num1 = int(query_result.get('parameters').get('number'))
+        num2 = int(query_result.get('parameters').get('number1'))
+        product = str(num1 * num2)
+        print('here num1 = {0}'.format(num1))
+        print('here num2 = {0}'.format(num2))
+        fulfillmentText = 'The product of the two numbers is ' + product
+    return {"fulfillmentText": fulfillmentText, "source": "webhookdata"}
