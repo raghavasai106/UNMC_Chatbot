2d3bd688667757e2061384d34a08eceb3c8bbae8
Raghavasai Kowlasker <rkowlasker@unomaha.edu>
2022-07-05 15:50:29 -0500

    added wsgi file.

---
+++ helloworld/__init__.py
@@ -0,0 +1,35 @@
+from urllib import request
+
+from flask import Flask
+app = Flask(__name__)
+@app.route("/")
+def hello():
+    return "Hello, Flask!"
+@app.route('/about')
+def welcome():
+    return "Welcome to my Flask site, ©2018"
+
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    req = request.get_json(silent=True, force=True)
+    fulfillmentText = ''
+    sum = 0
+    query_result = req.get('queryResult')
+    if query_result.get('action') == 'add.numbers':
+        num1 = int(query_result.get('parameters').get('number'))
+        num2 = int(query_result.get('parameters').get('number1'))
+        sum = str(num1 + num2)
+        print('here num1 = {0}'.format(num1))
+        print('here num2 = {0}'.format(num2))
+        fulfillmentText = 'The sum of the two numbers is ' + sum
+    elif query_result.get('action') == 'multiply.numbers':
+        num1 = int(query_result.get('parameters').get('number'))
+        num2 = int(query_result.get('parameters').get('number1'))
+        product = str(num1 * num2)
+        print('here num1 = {0}'.format(num1))
+        print('here num2 = {0}'.format(num2))
+        fulfillmentText = 'The product of the two numbers is ' + product
+    return {"fulfillmentText": fulfillmentText, "source": "webhookdata"}
+if __name__ == "__main__":
+    app.run()
